526e28f58f91e9f6b56be127055f9a6f5b718de6
abrajoe <fiftycent1@sms.at>
2012-11-30 17:46:35 +0100

    Added Soapservice, added Unity Config, added functionallities to BL in order to be able to add a Customer/Site 	new file:   CRMService/CRMService.csproj 	new file:   CRMService/CRMService.csproj.user 	new file:   CRMService/IServiceCRM.cs 	new file:   CRMService/Properties/AssemblyInfo.cs 	new file:   CRMService/ServiceCRM.svc 	new file:   CRMService/ServiceCRM.svc.cs 	new file:   CRMService/Web.Debug.config 	new file:   CRMService/Web.Release.config 	new file:   CRMService/Web.config 	new file:   CRMService/packages.config 	modified:   BusinessTest/BusinessTest.csproj 	modified:   BusinessTest/CustomerServiceTest.cs 	modified:   BusinessTest/SiteServiceTest.cs 	modified:   SKS.Scada.BL/CustomerService.cs 	modified:   SKS.Scada.BL/Interfaces/ICustomerService.cs 	modified:   SKS.Scada.BL/Interfaces/ISiteService.cs 	deleted:    SKS.Scada.BL/SKS.Scada.Business.csproj 	modified:   SKS.Scada.BL/SiteService.cs 	modified:   SKS.Scada.WebService/SiteService.svc.cs 	modified:   SKS.sln 	modified:   SKS/SKS.Scada.DAL.csproj 	modified:   packages/repositories.config

---
+++ C#/SKS/CRMService/IServiceCRM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SKS.Scada.DAL;
+
+namespace CRMService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceCRM
+    {
+        [OperationContract]
+        void AddSite(Customer custom, string description, double longitude, double latitude);
+
+        [OperationContract]
+        Customer AddCustomer(string firstname, string email, string lastname, string password, string username, Technician technician);
+
+        // TODO: Add your service operations here
+    }
+
+
+}
---
+++ C#/SKS/CRMService/ServiceCRM.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using SKS.Scada.DAL;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+
+namespace CRMService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class ServiceCRM : IServiceCRM
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public void AddSite(Customer custom, string description, double longitude, double latitude)
+        {
+
+            IUnityContainer unitycontainer = new UnityContainer();
+            unitycontainer.LoadConfiguration("UnityContainer");
+
+            SKS.Scada.BL.ISiteService siteserv = unitycontainer.Resolve<SKS.Scada.BL.ISiteService>();
+            siteserv.AddSite(custom, description, longitude, latitude);
+            
+        }        
+
+        public Customer AddCustomer(string firstname, string email, string lastname, 
+            string password, string username, Technician technician)
+        {
+            IUnityContainer unitycontainer = new UnityContainer();
+            unitycontainer.LoadConfiguration("UnityContainer");
+            SKS.Scada.BL.ICustomerService custserv = unitycontainer.Resolve<SKS.Scada.BL.ICustomerService>();
+            return custserv.AddCustomer(firstname, email, lastname,
+             password, username, technician);
+
+            
+        }
+
+        
+    }
+}
